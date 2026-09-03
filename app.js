3682f4d4f950eee0d33baa767906640d12cc70e9
Caleb Borge <borgecaleb69@gmail.com>
2026-09-03 12:46:02 -0700

    Deployment

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json()); // Parse JSON bodies
 
@@ -64,5 +66,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Server error!' });
 });
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
